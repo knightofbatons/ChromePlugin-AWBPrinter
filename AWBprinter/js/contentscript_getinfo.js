a05b5b5d4393a632d1ddf0ad27e4865c9922cc58
Knightofbatons <knightofbatons@hotmail.com>
2017-11-03 14:42:34 +0800

    整理目录

---
+++ AWBprinter/js/contentscript_getinfo.js
@@ -0,0 +1,19 @@
+window.localStorage.clear();
+airwaybillList = [];
+$("td[name=txtAwb]").each(function (b) {
+    spantxt = "";
+    spantxt = $(this).text();
+    airwaybillList.push(spantxt);
+});
+Array.prototype.unique3 = function () {
+    for (var b = [], c = {}, a = 0; a < this.length; a++) {
+        c[this[a]] || (b.push(this[a]), c[this[a]] = 1);
+    }
+    return b;
+};
+airwaybillList.push("THE END");
+window.localStorage.setItem("printAWB", !1);
+var airwaybillListUnique = airwaybillList.unique3();
+window.localStorage.setItem("AWBList", airwaybillListUnique);
+console.log("AWB Printer: We will search & print ---\x3e " + airwaybillListUnique);
+window.open("http://accostest.airchinacargo.com/AWB_Information.asp");
