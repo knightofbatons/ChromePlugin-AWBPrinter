ce8d7f269b04368315cc2d6a7d1eb43565443b79
Knightofbatons <knightofbatons@hotmail.com>
2017-11-15 15:54:58 +0800

    分出需要打印的电子运单

--- AWBprinter/js/contentscript_useinfo.js
+++ AWBprinter/js/contentscript_useinfo.js
@@ -1,9 +1,10 @@
 try {
     var airwaybillList = window.localStorage.getItem("AWBList").split(",");
+    chrome.extension.sendMessage(airwaybillList.length);
     if (0 < airwaybillList.length) {
-        chrome.extension.sendMessage(airwaybillList.length);
         var printAWB = window.localStorage.getItem("printAWB");
-        "true" === printAWB && (localStorage.setItem("printAWB", !1), document.getElementsByName("subprt")[0].click());
+        "true" === printAWB && (localStorage.setItem("printAWB", !1), "EAW" != document.getElementsByName("pspl1")[0].value && "EAW" != document.getElementsByName("pspl2")[0].value && "EAW" != document.getElementsByName("pspl3")[0].value && "EAW" != document.getElementsByName("pspl4")[0].value && "EAP" != document.getElementsByName("pspl1")[0].value && "EAP" != document.getElementsByName("pspl2")[0].value && "EAP" != document.getElementsByName("pspl3")[0].value && "EAP" != document.getElementsByName("pspl4")[0].value &&
+            "ECC" != document.getElementsByName("pspl1")[0].value && "ECC" != document.getElementsByName("pspl2")[0].value && "ECC" != document.getElementsByName("pspl3")[0].value && "ECC" != document.getElementsByName("pspl4")[0].value || document.getElementsByName("subprt")[0].click());
         setTimeout(function () {
             var a = airwaybillList.shift();
             window.localStorage.clear();
@@ -19,6 +20,5 @@ try {
         }, 2500);
     }
 } catch (a) {
-    console.log("AWB Printer: contentscript_useinfo won't run in this page ");
-    console.log(a);
+    console.log("AWB Printer: contentscript_useinfo won't run in this page "), console.log(a);
 };
